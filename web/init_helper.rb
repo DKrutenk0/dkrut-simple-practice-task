79510437c6447b1708c18a64d951bbe8276269d6
Dmytro Krutenko <102244367+DKrutenk0@users.noreply.github.com>
2024-05-16 15:32:00 +0300

    create init_helper for initializing gems and tools

---
+++ web/init_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative 'features/config/support/hooks.rb'
+require_relative '../Constants/random'
+require 'site_prism'
+require 'faker'
+require 'rspec'
+
+require_relative '../Tools/asserts'
+
+current_path = File.dirname(__FILE__)
+pages_path = File.join(current_path, 'pages')
+
+Dir[File.join(current_path + '/../Tools', '*.rb')].each { |file| require file }
+Dir[File.join(pages_path, '*.rb')].each { |file| require file }

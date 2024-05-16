b310bb5abfadfd69154ef30c33937b848f560e8c
Dmytro Krutenko <102244367+DKrutenk0@users.noreply.github.com>
2024-05-16 15:33:41 +0300

    create login_steps with wrapper

---
+++ web/features/step_definitions/login_steps.rb
@@ -0,0 +1,24 @@
+require_relative '../../init_helper.rb'
+
+Given("I am on the Login page") do
+  Web::Pages::LoginPage.new.wait_for_page_load
+  $logger.info("Open login page")
+end
+
+And("I use {string} method on {string} with member data") do |method_name, page_name|
+  get_page(page_name).send(method_name.to_sym, @test_member_data)
+  $logger.info("Triggered '#{method_name}' method on '#{page_name}' with member data: \n\n #{@test_member_data}")
+end
+
+And("I use {string} method on {string}") do |method_name, page_name|
+  get_page(page_name).send(method_name.to_sym)
+end
+
+And("I use {string} method on {string} using {string} value") do |method_name, page_name, value|
+  get_page(page_name).send(method_name.to_sym, value)
+  $logger.info("Triggered '#{method_name}' method on '#{page_name}' with '#{value}' value")
+end
+
+def get_page(page_name)
+  Object.const_get("Web::Pages::#{page_name.gsub('_', ' ').split.map(&:capitalize).join('')}").new
+end

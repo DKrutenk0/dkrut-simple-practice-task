698bd6d7e4bf20eb023f7a20d7b3ef9ac1dbae5e
Dmytro Krutenko <102244367+DKrutenk0@users.noreply.github.com>
2024-05-17 14:39:20 +0300

    update init_helper

--- features/support/config/init_helper.rb
+++ features/support/config/init_helper.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
+# features/support/config/init_helper.rb
 
 require_relative 'hooks.rb'
-require_relative '../../../Constants/random'
 require 'site_prism'
 require 'faker'
 require 'rspec'
+require 'capybara/cucumber'
+require 'capybara/dsl'
 
 require_relative '../../../Tools/asserts'
 
 current_path = File.dirname(__FILE__)
+constants_path = current_path + '/../../../Constants'
+tools_path = current_path + '/../../../Tools'
 pages_path = File.join(current_path + '/../../../Web', 'pages')
+generators_path = current_path + '/../../../Generators'
 
-Dir[File.join(current_path + '/../../../Tools', '*.rb')].each { |file| require file }
+Dir[File.join(constants_path, '*.rb')].each { |file| require file }
+Dir[File.join(tools_path, '*.rb')].each { |file| require file }
 Dir[File.join(pages_path, '*.rb')].each { |file| require file }
+Dir[File.join(generators_path, '*.rb')].each { |file| require file }

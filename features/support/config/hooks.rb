c60aa3a91eee982aa789c96dce49089d42faa883
Dmytro Krutenko <102244367+DKrutenk0@users.noreply.github.com>
2024-05-17 14:23:51 +0300

    fixed hooks

--- features/support/config/hooks.rb
+++ features/support/config/hooks.rb
@@ -1,39 +1,21 @@
 # frozen_string_literal: true
+# features/support/config/hooks.rb
 
 require 'logger'
 
 $logger = Logger.new(STDOUT)
 
-Before do
-  @constants = Constants::Random
-end
 
-Before do |scenario|
+Before('@web') do |scenario|
   $logger.info("Scenario '#{scenario.name}' started")
 end
 
-Before('@web') do
-  @driver = Capybara.current_session.driver.browser
-  @driver.manage.window.maximize
-  @driver.navigate.to "https://secure.simplepractice.com"
-
-  @test_member_data = { 'username' => 'somab63683@lewenbo.com',
-                        'password' => 'GoodLuck777',
-                        'client_type' => 'Adult',
-                        'first_name' => @constants.random_first_name,
-                        'last_name' => @constants.random_last_name,
-                        'email' => @constants.random_email,
-                        'phone' => @constants.random_phone }
-end
-
-After do |scenario|
+After('@web') do |scenario|
   if scenario.failed?
     $logger.info("Scenario '#{scenario.name}' failed!")
   else
     $logger.info("Scenario '#{scenario.name}' passed")
   end
-end
 
-After('@web') do
   Capybara.current_session.reset!
 end

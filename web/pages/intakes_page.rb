0e2e40781a8b00eca500bce2a4cbbfa10126d8ea
Dmytro Krutenko <102244367+DKrutenk0@users.noreply.github.com>
2024-05-17 14:43:40 +0300

    update intakes_page

--- web/pages/intakes_page.rb
+++ web/pages/intakes_page.rb
@@ -6,19 +6,23 @@ module Web
       element :continue_button, :xpath, '//div[contains(@class, "full-flyout-footer")]/div//button[@class="button primary"]'
       element :select_plan_link, :xpath, '//button[@class="button-link" and contains(text(), "plan")]'
       element :view_message_link, :xpath, "//a[@href='#']"
-      element :banner_warning_text_field, :xpath, '//div[contains(@class, "banner-warning")]'
       element :sending_email_notification_text_field, :xpath, '//*[contains(text(), "Sending email now")]'
 
       def tap_continue
-        element_visible?(:xpath, '//div[contains(@class, "banner-warning")]', 15)
+        wait_until_continue_button_visible
         continue_button.click
       end
 
       alias tap_submit tap_continue
 
+      def tap_continue_to_email
+        element_visible?(:xpath, '//a[@href="/practice_settings/note_templates/library"]', 15)
+        tap_continue
+      end
+
       def tap_continue_to_preview
         wait_until_select_plan_link_visible
-        continue_button.click
+        tap_continue
       end
 
       def tap_share_and_send
